4c38eb52a0e26dada325123e46566f151f4d6d2a
bojangjorgjievski4 <bojangjorgjievski399@gmail.com>
2023-12-14 18:33:25 +0100

    first commit

---
+++ src/project12/Func_a.java
@@ -0,0 +1,15 @@
+package project12;
+
+public class Func_a {
+
+	public static void main(String[] args) {
+int a=10;
+int b=myFunc (a);
+System.out.println(b);
+
+	}
+	public static int myFunc (int a) {
+		return a*3;
+	}
+
+}
---
+++ src/project12/Func_b.java
@@ -0,0 +1,18 @@
+package project12;
+
+public class Func_b {
+
+	public static void main(String[] args) {
+int a = 10;
+myFunc(a);
+
+
+	}
+
+	public static void myFunc (int a) {
+		int b;
+		b = a*3;
+		System.out.println(b);
+		
+	}
+}
